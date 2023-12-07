bac05f315f4816f9d2bd76b9d764a4c2c0f9972a
memoryleak47 <memoryleak47@protonmail.ch>
2023-12-08 00:18:57 +0100

    initial

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+   ip: String,
+   port: Option<u16>,
+   keys: Keys,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Keys {
+   github: String,
+   travis: Option<String>,
+}
+
+fn main() {
+    let config: Config = toml::from_str(r#"
+       ip = '127.0.0.1'
+
+       [keys]
+       github = 'xxxxxxxxxxxxxxxxx'
+       travis = 'yyyyyyyyyyyyyyyyy'
+    "#).unwrap();
+
+    assert_eq!(config.ip, "127.0.0.1");
+    assert_eq!(config.port, None);
+    assert_eq!(config.keys.github, "xxxxxxxxxxxxxxxxx");
+    assert_eq!(config.keys.travis.as_ref().unwrap(), "yyyyyyyyyyyyyyyyy");
+
+    println!("Hello, world!");
+}

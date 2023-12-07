e67a8046a86f86676f293bd6f494d956c2c42ee6
memoryleak47 <memoryleak47@protonmail.ch>
2023-12-08 00:34:30 +0100

    add minifb

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Read;
+use minifb::{Key, Window, WindowOptions};
 
 #[derive(Serialize, Deserialize)]
 struct Config {
@@ -17,6 +18,9 @@ struct Object {
     xy_offset: [f64; 2],
 }
 
+const WIDTH: usize = 640;
+const HEIGHT: usize = 360;
+
 fn main() {
     let mut f = File::open("input.toml").unwrap();
     let mut data = String::new();
@@ -26,4 +30,31 @@ fn main() {
     for obj in config.object {
         dbg!(obj);
     }
+
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 255 + (255 << 16) + (255 << 8); // write something more funny here!
+        }
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window
+            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap();
+    }
+
 }

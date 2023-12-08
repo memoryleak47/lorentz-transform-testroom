2aebba13fd3bd703aa08f507352ab3dc10baf178
memoryleak47 <memoryleak47@protonmail.ch>
2023-12-08 11:39:12 +0100

    start lorentz test suite

--- src/lorentz.rs
+++ src/lorentz.rs
@@ -8,14 +8,14 @@ pub struct Frame {
     pub xy_offset: [f64; 2], // if xy_offset[i] is very high, then most events (viewed from within this frame) will have a negative xy[i].
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 pub struct Event {
     pub xy: [f64; 2],
     pub t: f64,
 }
 
 impl Frame {
-    pub fn main() -> Frame {
+    pub const fn main() -> Frame {
         Frame {
             velocity: [0.0, 0.0],
             t_offset: 0.0,
@@ -81,5 +81,56 @@ fn lorentz_straight(velocity_x: f64, ev: Event, c: Option<f64>) -> Event {
     Event {
         xy: [x, y],
         t,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn assert_close(ev1: Event, ev2: Event) {
+        const T: f64 = 1e-5;
+        assert!((ev1.xy[0] - ev2.xy[0]).abs() <= T, "x error: {} vs. {}", ev1.xy[0], ev2.xy[0]);
+        assert!((ev1.xy[1] - ev2.xy[1]).abs() <= T, "y error: {} vs. {}", ev1.xy[1], ev2.xy[1]);
+        assert!((ev1.t - ev2.t).abs() <= T, "t error: {} vs. {}", ev1.t, ev2.t);
+    }
+
+    const A: Frame = Frame::main();
+    const B: Frame = Frame {
+        velocity: [30.0, 0.0],
+        xy_offset: [0.0, 0.0],
+        t_offset: 0.0,
+    };
+
+    const C: Frame = Frame {
+        velocity: [0.0, 30.0],
+        xy_offset: [0.0, 0.0],
+        t_offset: 0.0,
+    };
+
+    const EV1: Event = Event {
+        xy: [2.4, 83.0],
+        t: -24.0,
+    };
+
+    #[test]
+    fn refl_test() {
+        for frame in [A, B, C] {
+            let ev = EV1;
+            let ev = frame.from_other_frame(frame, ev, Some(100.0));
+            assert_close(ev, EV1);
+        }
+    }
+
+    #[test]
+    fn reversibility() {
+        for src in [A, B, C] {
+            for dst in [A, B, C] {
+                let ev = EV1;
+                let ev = dst.from_other_frame(src, EV1, Some(100.0));
+                let ev = src.from_other_frame(dst, ev, Some(100.0));
+                assert_close(ev, EV1);
+            }
+        }
     }
 }
